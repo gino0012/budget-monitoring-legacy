c17f1ac45d701d0779402d51e5a099c4738314b0
angular-cli <angular-cli@angular.io>
2017-02-07 21:37:24 +0800

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BudgetMonitoringPage } from './app.po';
+
+describe('budget-monitoring App', function() {
+  let page: BudgetMonitoringPage;
+
+  beforeEach(() => {
+    page = new BudgetMonitoringPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
