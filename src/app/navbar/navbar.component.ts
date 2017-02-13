d5daf760cc0f10397408650ee69903ff94aaeb3a
Gino Carlo Saavedra <gino.saavedra@nokia.com>
2017-02-13 15:24:53 +0800

    add constants class

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Constants } from '../shared/constants/constants';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [Constants]
 })
 export class NavbarComponent implements OnInit {
-  title = 'Budget Monitoring';
-  budgetTab = 'Budgets';
-  addButton = {
-    nav: 'add',
-    id: 'add-budget',
-    sideNav: 'Add New Budget'
-  };
+  title: string;
+  budgetTab: string;
+  addButton: Object;
 
-  constructor() { }
+  constructor(private constants: Constants) {
+    this.title = 'Budget Monitoring';
+    this.budgetTab = 'Budgets';
+    this.addButton = {
+      nav: 'add',
+      id: constants.ADD_BUDGET_ID,
+      sideNav: 'Add New Budget'
+    };
+  }
 
   ngOnInit() { }
 }
