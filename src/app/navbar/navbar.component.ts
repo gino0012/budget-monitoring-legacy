443f31a39349753d28f100c07bd98882e750bdbb
Gino Carlo Saavedra <gino.saavedra@nokia.com>
2017-02-13 18:11:22 +0800

    add google signin

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 import { Constants } from '../shared/constants/constants';
 
@@ -13,7 +13,8 @@ export class NavbarComponent implements OnInit {
   budgetTab: string;
   addButton: Object;
 
-  constructor(private constants: Constants) {
+  constructor(private constants: Constants, private ngZone: NgZone) {
+    window['onSignIn'] = (user) => ngZone.run(() => this.onSignIn(user));
     this.title = 'Budget Monitoring';
     this.budgetTab = 'Budgets';
     this.addButton = {
@@ -24,4 +25,12 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() { }
+
+  onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
 }
