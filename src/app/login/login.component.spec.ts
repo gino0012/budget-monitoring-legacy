8063c84ad648294f8deec3742e41586ffc6c59c5
Gino Carlo Saavedra <gino_0012@yahoo.com.ph>
2017-02-27 21:41:33 +0800

    add checking of authentication in login page

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,3 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
 import { Router } from '@angular/router';
@@ -11,31 +10,47 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let navigateSpy;
+  let isLoginSpy, navigateSpy;
 
   beforeEach(async(() => {
-    navigateSpy = jasmine.createSpy('navigate');
-    const mockRouter = {
-      navigate: navigateSpy
-    };
-
-    TestBed.configureTestingModule({
-      imports: [HttpModule],
-      declarations: [ LoginComponent ],
-      providers: [GoogleApiService, UserDataService,
-        { provide: Router, useValue: mockRouter }
-      ]
-    })
-    .compileComponents();
+    compileModule(false);
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not redirect to home page if not authenticated', async(() => {
+    expect(isLoginSpy).toHaveBeenCalled();
+    expect(navigateSpy).not.toHaveBeenCalled();
+  }));
+
+  it('should redirect to login if not authenticated', async(() => {
+    TestBed.resetTestingModule();
+    compileModule(true);
+
+    expect(isLoginSpy).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['/home']);
+  }));
+
+  function compileModule(isLogin) {
+    isLoginSpy = jasmine.createSpy('is login').and.returnValue(isLogin);
+    navigateSpy = jasmine.createSpy('navigate');
+    const mockRouter = { navigate: navigateSpy };
+    const mockUserDataService = { isLogin: isLoginSpy };
+
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      declarations: [ LoginComponent ],
+      providers: [GoogleApiService,
+        { provide: UserDataService, useValue: mockUserDataService },
+        { provide: Router, useValue: mockRouter }
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { GoogleService } from '../shared/services/google.service';
+import { UserDataService } from '../shared/services/user-data.service';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,14 @@ import { GoogleService } from '../shared/services/google.service';
 export class LoginComponent implements OnInit {
 
   constructor(private ngZone: NgZone,
-              private google: GoogleService) {
+              private google: GoogleService,
+              private userService: UserDataService,
+              private router: Router) {
     window['onSignIn'] = (user) => ngZone.run(() => google.authenticate(user));
   }
 
   ngOnInit() {
+    if(this.userService.isLogin()) this.router.navigate(['/home']);
   }
 
 }
