0cdae1a2e29fca7c4e9e15f1996281e7c9706c60
Gino Carlo Saavedra <gino.saavedra@nokia.com>
2017-02-27 10:27:02 +0800

    fix failing test

--- src/app/shared/services/google.service.spec.ts
+++ src/app/shared/services/google.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Observable } from 'rxjs/Rx';
+
 import { GoogleService } from './google.service';
 import { GoogleApiService } from './google-api.service';
 import { UserDataService } from './user-data.service';
 
 describe('GoogleService', () => {
+  const mockAccessToken = { access_token: 'sample-access-token123'};
   let getAccessTokenSpy, loginSpy;
 
   beforeEach(() => {
-    getAccessTokenSpy = jasmine.createSpy('get Access Token').and.returnValue({ subscribe: () => {} });
+    getAccessTokenSpy = jasmine.createSpy('get Access Token').and.returnValue(Observable.of(JSON.stringify(mockAccessToken)));
     loginSpy = jasmine.createSpy('login');
     const  mockGoogleApiService = {
       getAccessToken: getAccessTokenSpy
@@ -41,14 +44,13 @@ describe('GoogleService', () => {
 
     it('should login using access token', inject([GoogleService], (service: GoogleService) => {
       const mockGoogleCode = 'sample-google-code123';
-      mockGoogleUser = {
-        code: mockGoogleCode
-      };
+      mockGoogleUser = {code: mockGoogleCode };
+
 
       service.authenticate(mockGoogleUser);
 
       expect(getAccessTokenSpy).toHaveBeenCalledWith(mockGoogleCode);
-      expect(loginSpy).toHaveBeenCalled();
+      expect(loginSpy).toHaveBeenCalledWith(mockAccessToken.access_token);
     }));
 
     it('should not get access token when code is null', inject([GoogleService], (service: GoogleService) => {
