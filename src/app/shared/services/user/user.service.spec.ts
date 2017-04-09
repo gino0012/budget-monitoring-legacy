29f4a3f54e8e70e4d7bd75e4746ad7f9edc24859
Gino Saavedra <gino.saavedra@nokia.com>
2017-04-09 20:33:34 +0800

    refactor test to use mockGoogleService

--- src/app/shared/services/user/user.service.spec.ts
+++ src/app/shared/services/user/user.service.spec.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Rx';
 
 import { UserService } from './user.service';
 import { GoogleService } from '../google/google.service';
+import { MockGoogleService } from '../../test/mocks/mock-google-service';
 
 describe('UserService', () => {
   const mockAccessToken = {
@@ -20,24 +21,26 @@ describe('UserService', () => {
       verified_email: true,
       access_type: 'offline'
   };
-  let mockGoogleService, mockRouter;
+  let service, mockGoogleService, mockRouter;
 
   beforeEach(() => {
-    mockGoogleService = {
-      authenticate: jasmine.createSpy('authenticate').and.returnValue(Observable.of(JSON.stringify(mockAccessToken))),
-      isAuthenticated: jasmine.createSpy('is authenticated').and.returnValue(Observable.of(mockResAuthToken))
-    };
     mockRouter = {
       navigate: jasmine.createSpy('navigate')
     };
 
     TestBed.configureTestingModule({
       providers: [UserService,
-        { provide: GoogleService, useValue: mockGoogleService },
+        { provide: GoogleService, useClass: MockGoogleService },
         { provide: Router, useValue: mockRouter }
       ]
     });
   });
+
+  beforeEach(inject([UserService, GoogleService],
+    (_service_, _mockGoogleService) => {
+      service = _service_;
+      mockGoogleService = _mockGoogleService;
+    }));
 
   afterEach(() => {
     localStorage.clear();
@@ -50,24 +53,25 @@ describe('UserService', () => {
       mockGoogleUser = {
         code: 'sample-google-code123'
       };
+      mockGoogleService.authenticate.and.returnValue(Observable.of(JSON.stringify(mockAccessToken)));
     });
 
-    it('should login when google code is not null', inject([UserService], (service: UserService) => {
+    it('should login when google code is not null', () => {
       service.login(mockGoogleUser);
 
       expect(mockGoogleService.authenticate).toHaveBeenCalledWith(mockGoogleUser.code);
       expect(localStorage.getItem('access_token')).toBe(mockAccessToken.access_token);
       expect(mockRouter.navigate).toHaveBeenCalledWith(['/home']);
-    }));
+    });
 
-    it('should login when google code is not null', inject([UserService], (service: UserService) => {
+    it('should login when google code is not null', () => {
       mockGoogleUser.code = null;
       service.login(mockGoogleUser);
 
       expect(mockGoogleService.authenticate).not.toHaveBeenCalled();
       expect(localStorage.getItem('access_token')).toBe(null);
       expect(mockRouter.navigate).not.toHaveBeenCalled();
-    }));
+    });
   });
 
   describe('isLogin()', () => {
@@ -76,9 +80,10 @@ describe('UserService', () => {
     beforeEach(() => {
       authSuccessSpy = jasmine.createSpy('auth success');
       authFailedSpy = jasmine.createSpy('auth failed');
+      mockGoogleService.isAuthenticated.and.returnValue(Observable.of(mockResAuthToken));
     });
 
-    it('should check if already login', inject([UserService], (service: UserService) => {
+    it('should check if already login', () => {
       localStorage.setItem('access_token', mockAccessToken.access_token);
 
       service.isLogin().subscribe(authSuccessSpy, authFailedSpy);
@@ -86,22 +91,22 @@ describe('UserService', () => {
       expect(mockGoogleService.isAuthenticated).toHaveBeenCalledWith(mockAccessToken.access_token);
       expect(authSuccessSpy).toHaveBeenCalledWith(true);
       expect(authFailedSpy).not.toHaveBeenCalled();
-    }));
+    });
 
-    it('should check if not login when no saved access token', inject([UserService], (service: UserService) => {
+    it('should check if not login when no saved access token', () => {
       service.isLogin().subscribe(authSuccessSpy, authFailedSpy);
 
       expect(mockGoogleService.isAuthenticated).not.toHaveBeenCalled();
       expect(authSuccessSpy).not.toHaveBeenCalled();
       expect(authFailedSpy).toHaveBeenCalledWith(false);
-    }));
+    });
   });
 
   describe('getAccessToken()', () => {
-    it('should get access token', inject([UserService], (service: UserService) => {
+    it('should get access token', () => {
       localStorage.setItem('access_token', mockAccessToken.access_token);
 
       expect(service.getAccessToken()).toBe(mockAccessToken.access_token);
-    }));
+    });
   });
 });
