c258a7a93bc1ae43dae7020b03c9c8b76ef4c42c
Gino Saavedra <gino.saavedra@nokia.com>
2017-04-09 20:15:43 +0800

    create user data service

---
+++ src/app/shared/interfaces/user-data-interface.ts
@@ -0,0 +1,6 @@
+export interface UserDataInterface {
+  setAccessToken(value: string): void;
+  getAccessToken(): string;
+  setDataId(value: string): void;
+  getDataId(): string;
+}
---
+++ src/app/shared/services/user/user-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { UserDataInterface } from '../../interfaces/user-data-interface';
+
+@Injectable()
+export class UserDataService implements UserDataInterface{
+  private KEY = {
+    'ACCESS_TOKEN': 'access_token'
+  };
+  private dataId;
+
+  constructor() { }
+
+  setAccessToken(value: string): void {
+    localStorage.setItem(this.KEY.ACCESS_TOKEN, value);
+  }
+
+  getAccessToken(): string {
+    return localStorage.getItem(this.KEY.ACCESS_TOKEN);
+  }
+
+  setDataId(value: string): void {
+    this.dataId = value;
+  }
+
+  getDataId(): string {
+    return this.dataId;
+  }
+}
