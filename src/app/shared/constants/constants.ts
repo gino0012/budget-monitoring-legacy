3993fe33eed7e100617786290b7b51e140df91b4
Gino Saavedra <gino.saavedra@nokia.com>
2017-04-10 19:31:41 +0800

    add constant for accounts sheet name

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,4 +1,7 @@
 export class Constants {
   ADD_BUDGET_ID = 'add-budget';
   DATA_FILE_NAME = 'Budget Monitoring App Data (Don\'t remove or else all data from app will be gone)';
+  SHEET_NAME = {
+    ACCOUNTS: 'Accounts'
+  }
 }
