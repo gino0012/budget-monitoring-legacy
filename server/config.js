9558eb72f913253d02fa9e5673aa0137a54e34f4
Gino Saavedra <gino.saavedra@nokia.com>
2017-10-17 10:31:26 +0800

    change redirect url

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   CLIENT_ID: '861770303263-nhmpmupmg7je2d3u76714ij8dun527up.apps.googleusercontent.com',
   CLIENT_SECRET: 'fZtFjpzDmsm0ZWTxYOVnj94Z',
-  REDIRECT_URL: 'http://localhost:4200/login',
+  REDIRECT_URL: 'https://budget-monitoring.herokuapp.com/login',
   GRANT_TYPE: 'authorization_code',
   AUTH_TOKEN_URL: 'https://accounts.google.com/o/oauth2/token'
 };
