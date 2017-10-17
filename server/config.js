05dbfc284b31baa293dc7c1fffdf444e7b57c0d3
Gino Saavedra <gino.saavedra@nokia.com>
2017-10-17 10:44:30 +0800

    change client secret

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   CLIENT_ID: '861770303263-nhmpmupmg7je2d3u76714ij8dun527up.apps.googleusercontent.com',
-  CLIENT_SECRET: 'fZtFjpzDmsm0ZWTxYOVnj94Z',
+  CLIENT_SECRET: '6o8G0nBJt27Y56iwciAHmNmf',
   REDIRECT_URL: 'https://budget-monitoring.herokuapp.com/login',
   GRANT_TYPE: 'authorization_code',
   AUTH_TOKEN_URL: 'https://accounts.google.com/o/oauth2/token'
